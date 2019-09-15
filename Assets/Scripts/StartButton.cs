3ee42a6be3da7be3b6d2432472e34ff41b7c8c0b
Jumpei Terashita <genuineflat@gmail.com>
2019-09-15 21:55:01 +0900

    First Commit

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    [SerializeField]
+    public TaskItemScript baseItem;
+
+    private Text buttonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonText = GetComponentInChildren<Text>();
+
+        UpdateButtonText();
+    }
+
+    public void OnClick()
+    {
+        baseItem.ToggleTimer();
+
+        UpdateButtonText();
+    }
+
+    private void UpdateButtonText()
+    {
+        bool isTimerRunning = baseItem.isTimerRunning();
+
+        buttonText.text = isTimerRunning ? "Stop" : "Start";
+    }
+
+}
---
+++ Assets/Scripts/TaskItemScript.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class TaskItemScript : MonoBehaviour
+{
+    [SerializeField]
+    public Text timerText = null;
+
+    [SerializeField]
+    public Text taskText = null;
+
+    [SerializeField]
+    public int totalSecond = 0;
+
+    [SerializeField]
+    public string taskName;
+
+    [SerializeField]
+    private bool isRunning = false;
+
+    private int hour;
+    private int minute;
+    private float second;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        taskText.text = taskName;
+
+        int tmp = totalSecond;
+        hour = tmp / 3600;
+        tmp = tmp % 3600;
+        minute = tmp / 60;
+        second = tmp % 60;
+
+        UpdateDispStr();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRunning)
+        {
+            second += Time.deltaTime;
+
+            if (second >= 60.0f)
+            {
+                second -= 60.0f;
+                minute += 1;
+
+                if (minute >= 60)
+                {
+                    minute -= 60;
+                    hour += 1;
+                }
+
+
+            }
+
+            UpdateDispStr();
+        }
+
+       
+    }
+
+    private void UpdateDispStr()
+    {
+        string hourStr = hour.ToString("00");
+        string minuteStr = minute.ToString("00");
+        string secondStr = (Mathf.Floor(second)).ToString("00");
+
+
+        timerText.text = hourStr + ":" + minuteStr + ":" + secondStr;
+    }
+
+    public void ToggleTimer()
+    {
+        isRunning = !isRunning;
+    }
+
+    public bool isTimerRunning()
+    {
+        return isRunning;
+    }
+
+    public void ResetTimer()
+    {
+        hour = 0;
+        minute = 0;
+        second = 0;
+
+        UpdateDispStr();
+    }
+}
